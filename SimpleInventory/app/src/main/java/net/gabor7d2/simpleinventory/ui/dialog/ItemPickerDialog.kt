8484680eb5c221bfa849d34c23cfa15d954fa057
Gábor Tóth <gabor7d2@gmail.com>
2023-11-19 02:50:53 +0100

    Open keyboard when PickerDialog opens

--- SimpleInventory/app/src/main/java/net/gabor7d2/simpleinventory/ui/dialog/ItemPickerDialog.kt
+++ SimpleInventory/app/src/main/java/net/gabor7d2/simpleinventory/ui/dialog/ItemPickerDialog.kt
@@ -2,6 +2,7 @@ package net.gabor7d2.simpleinventory.ui.dialog
 
 import android.app.Dialog
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.DialogFragment
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
@@ -28,10 +29,13 @@ class ItemPickerDialog : DialogFragment() {
                 adapter.doSearch(text.toString())
             }
 
-            MaterialAlertDialogBuilder(it)
+            val dialog = MaterialAlertDialogBuilder(it)
                 .setTitle("Pick an item")
                 .setView(binding.root)
                 .create()
+
+            dialog.window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE)
+            dialog
         } ?: throw IllegalStateException("Context cannot be null")
     }
 }
